35e05b2822afaa2f4397794229a4e55dd50accb5
Jan Steinhauer <janst.geo@gmail.com>
2020-10-17 14:09:21 +0200

    Initial Commit

---
+++ TindboardCodeWithFriends/TindboardCodeWithFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TindboardCodeWithFriendsApp.swift
+//  TindboardCodeWithFriends
+//
+//  Created by Jan Steinhauer on 17.10.20.
+//
+
+import SwiftUI
+
+@main
+struct TindboardCodeWithFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
